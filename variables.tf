732839ab4359a4b10820da1aeddb1bac357a8de6
PushpaShreeBellamkonda <pushpashreebellamkonda@gmail.com>
2025-02-06 22:25:02 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -54,3 +54,115 @@ variable "public_subnet_cidrs_tags" {
   type = map
   default = {}
 }
+
+# Private subnet variables
+
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.private_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid private subnet CIDR"
+    }
+}
+
+
+variable "private_subnet_cidrs_tags" {
+  type = map
+  default = {}
+}
+
+# Database subnet variables
+
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.database_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid database subnet CIDR"
+    }
+}
+
+
+variable "database_subnet_cidrs_tags" {
+  type = map
+  default = {}
+}
+
+# NAT gate way
+variable "nat_gateway_tags" {
+    type = map
+    default = {}
+  
+}
+
+# public route table
+
+variable "public_route_table_tags" {
+  type = map
+  default = {}
+}
+
+# private route table
+
+variable "private_route_table_tags" {
+  type = map
+  default = {}
+}
+
+# database route table
+
+variable "database_route_table_tags" {
+  type = map
+  default = {}
+}
+
+# public route
+
+resource "aws_route" "public_route" {
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.ic
+}
+
+# private route
+
+resource "aws_route" "private_route_nat" {
+    route_table_id = aws_route_table.private.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+}
+
+# database route
+
+resource "aws_route" "database_route_nat" {
+    route_table_id = aws_route_table.database.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+}
+
+
+# public route table association
+
+resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = element(aws_subnet.public[*].id , count.index)
+    route_table_id = aws_route_table.public.id
+  
+}
+
+# private route table association
+
+resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidrs)
+    subnet_id = element(aws_subnet.private[*].id , count.index)
+    route_table_id = aws_route_table.private.id
+  
+}
+
+# database route table association
+
+resource "aws_route_table_association" "database" {
+    count = length(var.database_subnet_cidrs)
+    subnet_id = element(aws_subnet.database[*].id , count.index)
+    route_table_id = aws_route_table.database.id
+  
+}
--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,5 @@
+# VPC creation
+
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
@@ -10,6 +12,8 @@ resource "aws_vpc" "main" {
     }
   )
 }
+
+# Internet gate way
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
@@ -37,7 +41,108 @@ resource "aws_subnet" "public" {
     var.common_tags ,
     var.public_subnet_cidrs_tags ,
     {
-      Name = "${local.resource_name}-${local.az_names[count.index]}"
+      Name = "${local.resource_name}-public-${local.az_names[count.index]}"
+    }
+  )
+}
+
+
+# Private subnet
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  availability_zone = local.az_names[count.index]
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+
+   tags = merge(
+    var.common_tags ,
+    var.private_subnet_cidrs_tags ,
+    {
+      Name = "${local.resource_name}-private-${local.az_names[count.index]}"
+    }
+  )
+}
+
+# Database subnet
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  availability_zone = local.az_names[count.index]
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+
+   tags = merge(
+    var.common_tags ,
+    var.database_subnet_cidrs_tags ,
+    {
+      Name = "${local.resource_name}-database-${local.az_names[count.index]}"
+    }
+  )
+}
+
+# Nat gate way
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags , 
+    var.nat_gateway_tags , 
+    {
+      Name = "${local.resource_name}"   #expense-dev
+    }
+  )
+# To ensure proper ordering , it is reccomended to add an explicit dependency
+# on the IGW for the vpc
+  depends_on = [ aws_internet_gateway.gw ]  #this is explicit dependency
+}
+
+
+#  Public Route Table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags ,
+    var.public_route_table_tags,
+    {
+      Name = "${local.resource_name}-public"  #expense-dev
+    }
+  )
+}
+
+
+# Private route table
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags ,
+    var.private_route_table_tags,
+    {
+      Name = "${local.resource_name}-private"  #expense-dev
+    }
+  )
+}
+
+# database route table
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags ,
+    var.database_route_table_tags,
+    {
+      Name = "${local.resource_name}-database"  #expense-dev
     }
   )
 }
